c003b26b96b4dce40220a7a923e4390e3029c8ab
frehlid <frehlid@student.ubc.ca>
2022-06-30 10:51:57 -0700

    more progress on dashboard component, commit does not complie

---
+++ Web_Client/instrumentation-web-app/src/Components/Dashboard/DashView.js
@@ -0,0 +1,26 @@
+import React, { useState, useContext } from "react";
+import SocketContext from "../../SocketContext.js";
+
+function DashView(props) {
+
+	// get the socket from the context
+	const socket = useContext(SocketContext);
+
+	// useState to store the device, state is managed by the useEffect hook
+	// initial value is an empty object
+	const [device, setDevice] = useState({});
+
+	// useEffect runs on first render:
+	//  - request device data from server and subscribe for updates
+	//  - render a widget component for Temperature, pH, and dissolved oxygen
+
+
+	return (
+		<div classname="device-dashboard">
+			<h2>{props.device.name}</h2>
+			<p>{props.device.description}</p>
+		</div>
+	)
+}
+
+export default DashView;
--- Web_Client/instrumentation-web-app/src/Components/Dashboard/Dashboard.js
+++ Web_Client/instrumentation-web-app/src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
-import react, { useContext, useEffect, useState } from "React";
+import react, { useContext, useEffect, useState, useCallback } from "React";
 import SocketContext from "../../SocketContext.js";
-import Socket from "../../Socket.js"
+import DashView from "./DashView.js";
 
 // Dashboard container
 // -- Render title: Web-Brew
@@ -13,15 +13,34 @@ function Dashboard() {
     // get the socket from the context
     const socket = useContext(SocketContext);
 
-    // useEffect 
-    useEffect(() => {
+	// useState to store the list of devices, state is managed by the useEffect hook
+	// initial value is an empty array
+	const [devices, setDevices] = useState([]);
 
-    })
+	// callback for device list received
+	// useCallback is used to create a memoized version of the callback function -- only changes when the dependencies change
+	const deviceListCallback = useCallback((device_in) => {
+		setDevices(device_in);
+	}, [devices]);
+
+    // useEffect runs on first render:
+	//  - request device list from server and subscribe for updates
+	//  - render a dash-view component for each device
+	//  - render a message if no devices are connected
+    useEffect(() => {
+		socket.emit("request-device-list");
+		socket.on("device-list", deviceListCallback);
+    });
 
     
     return (
         <div classname="main-container">
-            
+            <h1>Web-Brew</h1>
+			<div classname="device-list">
+				{devices.map((device) => {
+					return <DashView device={device} />
+				})}
+			</div>
         </div>
     )
 }
